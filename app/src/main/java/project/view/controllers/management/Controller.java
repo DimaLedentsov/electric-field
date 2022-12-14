9d58c84246098df6f65293d57b7b9c9631a02034
DimaLedentsov <ledentd@mail.ru>
2022-12-15 02:53:39 +0300

    misc fix

--- app/src/main/java/project/view/controllers/management/Controller.java
+++
@@ -1,9 +0,0 @@
-package project.view.controllers.management;
-
-import javafx.application.Application;
-import project.App;
-@Deprecated
-public interface Controller {
-    public void setApp(App app);
-    public App getApp();
-}
